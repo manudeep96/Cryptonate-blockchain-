6e715fe301f5284429f66f5fec45a4942f68e092
Tanvi Karennavar <tanvikarennavar@Tanvis-MacBook-Pro.local>
2022-11-14 15:19:35 -0500

    SC calls

--- cryptonate-app/cryptonate-front-end/src/views/SignupPage.jsx
+++ cryptonate-app/cryptonate-front-end/src/views/SignupPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import {
   Typography,
   Box,
@@ -10,18 +10,42 @@ import {
 } from "@mui/material";
 import { Stack } from "@mui/system";
 import { useNavigate } from "react-router-dom";
+import Web3 from "web3";
+import NFTContractBuild from './Cryptonate.json';
 
 const LandingPage = () => {
-  const navigate = useNavigate();
-  const [signupAsCharity, setSignupAsCharity] = useState(false);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-
-  const signup = () => {
+    const navigate = useNavigate();
+    const [signupAsCharity, setSignupAsCharity] = useState(false);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    // const [accounts, setAccount] = useState('');
+    
+    
+    // Displays a prompt for the user to select which accounts to connect
+    async function requestAccount() {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      // setAccount(account[0]);
+    }
+  
+  const signup = async () => {
     console.log("Signing up");
     // Call sc
-    // Take the user to landing page again
-    navigate("/");
+    requestAccount();
+
+    let provider = window.ethereum;
+    let selectedAccount;
+    provider
+        .request({method: 'eth_requestAccounts' })
+        .then((accounts) => {
+          selectedAccount = accounts[0];
+          const web3 = new Web3(provider);
+          const nftContract = new web3.eth.Contract(NFTContractBuild.abi,'0xc703115d295A4CA319E8FeCc3f8d107cCb2e0F1A');
+          const sent = signupAsCharity ? nftContract.methods.registerCharity(selectedAccount).send({from: selectedAccount}) : nftContract.methods.registerDonor(selectedAccount).send({from: selectedAccount}) ;
+          console.log("yesss",sent);
+          console.log("account", name);
+          // Take the user to landing page again
+          navigate("/");
+        });
   };
 
   const handleCheckboxChange = (e) => {
